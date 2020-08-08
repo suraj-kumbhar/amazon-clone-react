f84d02d9fd035d14e1408a18da19cbbd6af32072
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-08-08 16:19:18 +0530

    build the banner

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <img
+                className="home__image"
+                src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/AugART/GW/Hero/ShopNow/fst/rec/DesktopHero_1.1x._CB408378457_.jpg"
+                alt="not available"
+            />
+        </div>
+    );
+}
+
+export default Home;
