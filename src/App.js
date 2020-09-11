50675ae8264c8b92ba07ae52c1112b68f27518b6
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-09-11 16:15:26 +0530

    added hover effect on the products

--- src/App.js
+++ src/App.js
@@ -6,53 +6,53 @@ import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
 import { useStateValue } from './StateProvider';
-import {auth } from './Firebase';
+import { auth } from './Firebase';
 
 function App() {
-    const [{user},dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((authUser) => {
-            if (authUser) {
-                dispatch({
-                    type: 'SET_USER',
-                    user: authUser,
-                })
-            } else {
-                dispatch({
-                    type: 'SET_USER',
-                    user: null,
-                })
-            }
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
         });
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        });
+      }
+    });
 
-        return () => {
-            unsubscribe();
-        };
-    },[dispatch]);
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]);
 
-    // console.log(user);
-    console.log('User is >>>>',user);
+  // console.log(user);
+  console.log('User is >>>>', user);
 
-    return (
-        <Router>
-            <div className="app">
-                <Switch>
-                    <Route path="/checkout">
-                        <Header />
-                        <Checkout />
-                    </Route>
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/">
-                        <Header />
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
